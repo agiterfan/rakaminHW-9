dd72c39ff061b8e0831a4ae82f21ddc6420b32a5
agiterfan <agita.erfan001@gmail.com>
2023-12-13 20:41:08 +0700

    push HW-9

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config = {
+  development: {
+    username: 'postgres',
+    password: '1997',
+    database: 'coba_movies',
+    host: 'localhost',
+    dialect: 'postgres'
+  }
+}
+
+module.exports = config;
